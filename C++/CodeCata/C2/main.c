a052524cf2909aa84b0d8d5358ace79eff428b42
osh010420 <qudtls0420@Naver.com>
2026-03-05 09:51:18 +0900

    123
    123

---
+++ C++/CodeCata/C2/main.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = num1 * num2;
+
+    if (0 <= num1 <= 100 && 0 <= num2 <= 100)
+        return answer;
+}
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    printf("%d", solution(num1, num2));
+    return 0;
+}
